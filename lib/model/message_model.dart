7cceddfec5cd9d4142b8296bd3a4b3db617cfe0d
rcshpark <rcshpark@gmail.com>
2023-03-18 21:04:50 +0900

    enum 변수 추가 및 불필요 코드 삭제

--- lib/model/message_model.dart
+++ lib/model/message_model.dart
@@ -1,18 +1,12 @@
+enum MessageType { user, bot }
+
 class MessageModel {
   late String userMessage;
   late String botMessage;
-  // late int answerCounting;
+  late MessageType messageType;
 
-  //테스트 모델
-
-  // late String user_id;
-  // late String email;
-
-  MessageModel({required this.userMessage, required this.botMessage});
-  // BoardModel({user_id, email});
-
-  MessageModel.fromJson(Map<String, dynamic> json) {
-    userMessage = json['userMessage'];
-    botMessage = json['botMessage'];
-  }
+  MessageModel(
+      {required this.userMessage,
+      required this.botMessage,
+      required this.messageType});
 }
